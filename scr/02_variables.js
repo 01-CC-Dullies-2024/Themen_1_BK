847c24e3f5abae0839abe444f25bea672bf4463a
BKUE136 <155626099+BKUE136@users.noreply.github.com>
2024-01-09 10:17:03 +0000

    02_variables ++

---
+++ scr/02_variables.js
@@ -0,0 +1,21 @@
+// Zeilenkommentar
+
+/*
+Blockkommentar
+this is a very long commentary
+this is a very long commentary
+this is a very long commentary
+this is a very long commentary
+*/
+
+/* variables 01*/
+
+// console.log("Hello, World!") //Ausgabe
+
+let Hi;
+Hi = "Hello, World!";
+console.log(Hi);
+
+let firstName; //declaration
+firstName = "Max" //assignment
+console.log(firstName); //Ausgabe
