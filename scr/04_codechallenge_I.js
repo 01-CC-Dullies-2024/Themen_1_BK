2a48423776b0b8af44f5ded232b2f39a4f944194
BKUE136 <155626099+BKUE136@users.noreply.github.com>
2024-01-10 10:20:30 +0000

    sumab ++

--- scr/04_codechallenge_I.js
+++ scr/04_codechallenge_I.js
@@ -10,3 +10,10 @@ Geben Sie die Summe der Zahlen in die Konsole aus:
 */
 
 const prompt = require('prompt-sync')({sigint: true});
+
+let a = parseInt(prompt ("Wie Groß ist die Zahl a? "));
+let b = parseInt(prompt ("Wie Groß ist die Zahl b? "));
+let sumab = parseInt(a+b)
+
+console.log ("Die Summe der Zahlen " + a + " und " + b + " ist " + (a+b));
+console.log (sumab)
