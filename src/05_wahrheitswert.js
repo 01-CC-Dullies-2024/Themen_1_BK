5c414b82324d37200563b39c9071743b24b56c19
BKUE136 <155626099+BKUE136@users.noreply.github.com>
2024-01-11 08:29:57 +0000

    wahrheitswerte

--- src/05_wahrheitswert.js
+++ src/05_wahrheitswert.js
@@ -19,3 +19,13 @@ ageMark = year - birthYearMark
 console.log("John's Age is: " + ageJohn);
 console.log("Mark's Age is: " + ageMark);
 console.log("Datum: " + date);
+
+let isJohnOlder, isJohnequal, isJohnYounger;
+
+isJohnOlder = (ageJohn > ageMark);
+isJohnequal = (ageJohn == ageMark);
+isJohnYounger = (ageJohn < ageMark);
+
+console.log("Ist John älter? " + isJohnOlder("Ja", "Nein"));
+console.log("Ist John gleichalt? " + isJohnequal("Ja", "Nein"));
+console.log("Ist John jünger? " + isJohnYounger("Ja", "Nein"));
