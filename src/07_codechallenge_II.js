01ef1b286a5317751088eb3e85fa6b96ed7bf3e3
BKUE136 <155626099+BKUE136@users.noreply.github.com>
2024-01-11 14:35:13 +0000

    if, else, jasper++

--- src/07_codechallenge_II.js
+++ src/07_codechallenge_II.js
@@ -24,20 +24,74 @@ const age =  parseInt (prompt ("Bitte Alter eingeben: ")); // "2" --> 2
 
 // console.log(age);
 
-switch (true) {
-    case (age >= 0) && (age <= 5) :
-        console.log(firstName + " trinkt Milch.");
-        break;
-    case (age >= 6) && (age <= 12):
-        console.log(firstName + " trinkt Saft" + " oder" +  " trinkt Milch");
-        break;
-    case (age >= 13) && (age <= 17):
-        console.log(firstName + " trinkt Cola" + " oder" +  " trinkt Saft" + " oder" +  " trinkt Milch");
-        break;
-    case (age >= 18) && (age <= 130):
-        console.log(firstName + " trinkt Wein" + " oder" +  " trinkt Cola" + " oder" +  " trinkt Saft" + " oder" +  " trinkt Milch");
-        break;   
-    default:
-        console.log("Bitte Tee in den oberen Input einführen!");   
-        break;
+// switch (true) {
+//     case (age >= 0) && (age <= 5) :
+//         console.log(firstName + " trinkt Milch.");
+//         break;
+//     case (age >= 6) && (age <= 12):
+//         console.log(firstName + " trinkt Saft" + " oder" +  " trinkt Milch");
+//         break;
+//     case (age >= 13) && (age <= 17):
+//         console.log(firstName + " trinkt Cola" + " oder" +  " trinkt Saft" + " oder" +  " trinkt Milch");
+//         break;
+//     case (age >= 18) && (age <= 130):
+//         console.log(firstName + " trinkt Wein" + " oder" +  " trinkt Cola" + " oder" +  " trinkt Saft" + " oder" +  " trinkt Milch");
+//         break;   
+//     default:
+//         console.log("Bitte Tee in den oberen Input einführen!");   
+//         break;
+// }
+
+
+// if-else-statement
+
+// console.log("Datentyp: " + typeof age + " Alter: " + age);
+
+isAgeless6 = (age < 6);
+isAgeLess13 = (age < 13);
+isAgeLess18 = (age < 18);
+isAgeless131 = (age < 131);
+
+isAgeNotNumber = typeof(age == Number(false));
+
+isNameJasper = (firstName == "Jasper");
+
+if (isAgeNotNumber) 
+{   
+    {
+        console.log(firstName + ", Bitte Zahl angeben! ");
+    }
+}
+
+if (isNameJasper) 
+{   
+    {
+        console.log(firstName + " trinkt ausschließlich Monster. ");
+    }
+}
+else if (isAgeless6) 
+{
+    console.log(firstName + " trinkt Milch.")
+}
+else if (isAgeLess13) 
+{   
+    {
+        console.log(firstName + " trinkt Saft" + " oder" + " trinkt Milch.");
+    }
+}
+else if (isAgeLess18) 
+{   
+    {
+        console.log(firstName + " trinkt Cola" + " oder" +  " trinkt Saft" + " oder" +  " trinkt Milch. ");
+    }
+}
+else if (isAgeless131) 
+{   
+    {
+        console.log(firstName + " trinkt Wein" + " oder" +  " trinkt Cola" + " oder" +  " trinkt Saft" + " oder" +  " trinkt Milch. ");
+    }
+}
+else
+{
+    console.log("Du bist so alt geworden, du darfst alles trinken, was du willst, sogar Korn.");
 }
