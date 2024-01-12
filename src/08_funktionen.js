752b8016bbeb7c4493bf62f18a5ec239b31a0e7b
BKUE136 <155626099+BKUE136@users.noreply.github.com>
2024-01-12 11:16:21 +0000

    ausgabeNameSRP++

--- src/08_funktionen.js
+++ src/08_funktionen.js
@@ -18,7 +18,7 @@ funktion
 // function ausgabeNamen()
 
 // {
-//     let firstName = prompt ("Bitte Namen eingeben: ");
+//     let firstName = prompt ("Bitte Namen eingeben: "); // Dieser Aufruf ist rekursiv
 //     console.log("Hallo Du da, " + firstName +"!");
 // }
 
@@ -30,18 +30,35 @@ funktion
 
 // let firstName = prompt ("Bitte Namen eingeben: ");
 
-// ausgabeNamenParams("Max", " Mütze")
-
 // function ausgabeNamen2(firstName)
 // {
 // console.log("Hallo, "+ firstName + "!")
 // }
 
-const prompt = require('prompt-sync')({sigint: true});
-ausgabeNamenParams(prompt ("Bitte Vornamen eingeben: "), prompt ("Bitte Nachnamen eingeben: ")); //Piping
+// const prompt = require('prompt-sync')({sigint: true});
+// ausgabeNamenParams(prompt ("Bitte Vornamen eingeben: "), prompt ("Bitte Nachnamen eingeben: ")); //Piping
 
-function ausgabeNamenParams(firstName, familyName)
+// function ausgabeNamenParams(firstName, familyName)
+// {
+//         console.log("Hallo, "+ firstName + " " + familyName + "!")
+// }
+// ;
+
+/***** Funktionen 03a *****/
+// 03a. Vorbereitung -Trennen der Funktionalitäten
+// Postulat: one function = one job (uncle Bob)
+// SRP single responsibility principle
+
+const prompt = require('prompt-sync')({sigint: true});
+// ausgabeNamenParamsSRP("Max", " Mütze")
+ausgabeNamenParamsSRP(prompt ("Bitte Vornamen eingeben: "), prompt ("Bitte Nachnamen eingeben: ")); //Piping
+
+function ausgabeNamenParamsSRP(firstName, familyName)
 {
-        console.log("Hallo, "+ firstName + " " + familyName + "!")
+    // 1.Funktionalität: string composing
+    const GAP = " ";
+    let word = "Hallo," + GAP + firstName + GAP + familyName + "!"
+    // 2. Funktionalität: string output
+    console.log(word)
 }
 ;
