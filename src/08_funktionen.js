9b6cc374a2a8fabbb1d879347c9b66f2a9194436
BKUE136 <155626099+BKUE136@users.noreply.github.com>
2024-01-12 10:52:36 +0000

    Ausgabe Param++

--- src/08_funktionen.js
+++ src/08_funktionen.js
@@ -5,8 +5,7 @@ funktion
 // funkionsaufruf | call
 
 // test();
-const prompt = require('prompt-sync')({sigint: true});
-ausgabeNamen()
+// ausgabeNamen()
 
 // function test()
 
@@ -14,12 +13,35 @@ ausgabeNamen()
 //     console.log("Hallo, Du da. ");
 // }
 
-
 // 2a. Parametrisierung + Datenübergabe von INNEN
 
-function ausgabeNamen()
+// function ausgabeNamen()
 
+// {
+//     let firstName = prompt ("Bitte Namen eingeben: ");
+//     console.log("Hallo Du da, " + firstName +"!");
+// }
+
+// 2b. Parametrisierung + Datenübergabe von Außen
+
+// ausgabeNamen2("Maria"); //Argument
+// ausgabeNamen2("Marim");
+// ausgabeNamen2("Madim");
+
+// let firstName = prompt ("Bitte Namen eingeben: ");
+
+// ausgabeNamenParams("Max", " Mütze")
+
+// function ausgabeNamen2(firstName)
+// {
+// console.log("Hallo, "+ firstName + "!")
+// }
+
+const prompt = require('prompt-sync')({sigint: true});
+ausgabeNamenParams(prompt ("Bitte Vornamen eingeben: "), prompt ("Bitte Nachnamen eingeben: ")); //Piping
+
+function ausgabeNamenParams(firstName, familyName)
 {
-    let firstName = prompt ("Bitte Namen eingeben: ");
-    console.log("Hallo Du da, " + firstName +"!");
+        console.log("Hallo, "+ firstName + " " + familyName + "!")
 }
+;
