fd03f98d8b7497e41a1d095f89cb5fef303f2fa6
BKUE136 <155626099+BKUE136@users.noreply.github.com>
2024-01-15 09:26:08 +0000

    rechenop ++

--- src/09_funktionen_II.js
+++ src/09_funktionen_II.js
@@ -10,10 +10,59 @@
 4. Ausgabe in Konsole : 
 */
 
+const prompt = require('prompt-sync')({sigint: true}); //wir brauchen prompt sync zur dateneingabe
 
+// Dateneingabe Zahl 1 + Zahl 2
 
+// const firstnumber = prompt ("Bitte erste Zahl eingeben: ");
+// let zahlA;
+//         while (isNaN(zahlA))
+//         {
+// 			zahlA =  parseInt (prompt ("Bitte eine Zahl eingeben: "));
+//         };
 
+// const secondnumber = prompt ("Bitte zweite Zahl eingeben: ");
+// let zahlB;
+// 		while (isNaN(zahlB))
+// 		{
+// 			zahlB =  parseInt (prompt ("Bitte eine Zahl eingeben: "));
+// 		};
+		
+// Auswahl der Rechenoperation 
+// Dateneingabe
 
+// function add //
+// output(sub)
+// output(multi)
+
+output(add(2,2))
+output(add(2,-2))
+output(add(2,0))
+
+output(sub(2,2))
+output(sub(2,-2))
+output(sub(2,0))
+
+output(multi(2,2))
+output(multi(2,-2))
+output(multi(2,0))
+
+function add(a, b) {
+	return a + b ;
+}
+
+function sub(a, b) {
+	return a - b ;
+}
+
+function multi(a, b) {
+	return a * b ;
+}
+
+// // function multi
+// function multi(a, b) {
+// 	return 3
+// }
 
 
 
@@ -23,4 +72,3 @@
 function output(outputData) {
 	console.log(outputData);
 }
-  
