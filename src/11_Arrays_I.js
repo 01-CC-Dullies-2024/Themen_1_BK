538f966a33c213ddbf3178a5f10d56b0c22e34a4
BKUE136 <155626099+BKUE136@users.noreply.github.com>
2024-01-16 10:40:16 +0000

    del spaces n empty rows in 11_arrays_I.js

--- src/11_Arrays_I.js
+++ src/11_Arrays_I.js
@@ -1,4 +1,3 @@
-
 /* Die Satzbau-Maschine | Arrays */
 
 /**
@@ -6,37 +5,29 @@
  * 
  */
 
-
-
 /**
  *   PRAXIS
  * 
  */
 
-
 /*** 01. Funktionalität mit Einzelparametern */
 // --> "Ich bin Max Mütze."
-// output(getSentence("Ich","bin","Max","Mütze"));
-// output(getSentence("Ich","bin","die","coole","Maxine","Mützerich"));
+output(getSentence("Ich","bin","Max","Mütze"));
+output(getSentence("Ich","bin","die","coole","Maxine","Mützerich"));
 function getSentence(word1,word2,word3,word4) {
     const GAP = " ";
     const PUNCT = ".";
     let str =   word1 + GAP +
                 word2 + GAP +
                 word3 + GAP +
-                word4 + GAP +
+                word4 +
                 PUNCT;
 
     return str;
 }
-
-
-
 
 // Modul: Ausgabe in Konsole : Test
 // output("hi"); 
 function output(outputStr) {
     console.log(outputStr);
 }
-
-
