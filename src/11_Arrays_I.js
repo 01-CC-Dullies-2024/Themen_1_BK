8b5d3c38c917530ba879f09d3ee7f465332f8cb3
BKUE136 <155626099+BKUE136@users.noreply.github.com>
2024-01-16 11:31:27 +0000

    arrays++

--- src/11_Arrays_I.js
+++ src/11_Arrays_I.js
@@ -1,33 +1,45 @@
 /* Die Satzbau-Maschine | Arrays */
 
-/**
- *   THEORIE
- * 
- */
+/***   THEORIE *  */
 
-/**
- *   PRAXIS
- * 
- */
+/***   PRAXIS *  */
 
-/*** 01. Funktionalität mit Einzelparametern */
-// --> "Ich bin Max Mütze."
-output(getSentence("Ich","bin","Max","Mütze"));
-output(getSentence("Ich","bin","die","coole","Maxine","Mützerich"));
-function getSentence(word1,word2,word3,word4) {
+/*** 01. Funktionalität mit Einzelparametern | test: "Ich bin Max Mütze." */
+
+// output(getSentence("Ich","bin","Max","Mütze"));
+output(getSentenceArr(["Ich","bin","die","coole","Maxine","Mützerich"]));
+
+function getSentenceArr(arr) {
     const GAP = " ";
     const PUNCT = ".";
-    let str =   word1 + GAP +
-                word2 + GAP +
-                word3 + GAP +
-                word4 +
+    let str =   arr[0] + GAP +
+                arr[1] + GAP +
+                arr[2] + GAP +
+                arr[3] + GAP +
+                arr[4] + GAP +
+                arr[5] + GAP +
+                arr[6] +
                 PUNCT;
 
     return str;
 }
 
+/*** 02. Array ***/
+
+// let arr; 
+// arr =new array(); //Konstruktor
+// arr =[]; // Literal
+// arr =[2,1,7,8,10];
+
+// output(arr);
+// output(arr.length);
+// output(arr[0]);
+// output(arr[4]);
+// output(arr[arr.length-1]);
+
+
 // Modul: Ausgabe in Konsole : Test
-// output("hi"); 
+// output("hi");
 function output(outputStr) {
     console.log(outputStr);
 }
