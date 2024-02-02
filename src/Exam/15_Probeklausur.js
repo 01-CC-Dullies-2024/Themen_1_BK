e8febb0280a77e0f526191fa5a8a356878ae95b8
BKUE136 <155626099+BKUE136@users.noreply.github.com>
2024-02-02 19:02:44 +0100

    Probeklausur

--- src/Exam/15_Probeklausur.js
+++ src/Exam/15_Probeklausur.js
@@ -7,46 +7,38 @@ Erzeugen Sie ein Array testArr mit 4 beliebigen Elementen vom Datentyp String.
 Geben Sie anschließend das letzte Element in die JS-Konsole aus. 
 */
 
+let testArr = ["Eins", "Zwei", "Drei", "Vier"];
+console.log(testArr);
+
+
 /*
 Aufgabe 2 (4 Punkte)
-Sie sehen folgende Struktur in Ihrem JS-Editor:
-var struc =
-{
- level1:
- {
- level2:
- {
-param1: 3.14,
-param2: 10.01
- }
- }
-};
-Geben Sie jeweils den Inhalt der property param1 und param2 in die JS-Console aus.
+Sie sehen folgende Struktur in Ihrem JS-Editor und 
+geben Sie jeweils den Inhalt der property param1 und param2 in die JS-Console aus.:
 */
+var struc =
+    {
+    level1:
+        {
+        level2:
+            {
+            param1: 3.14,
+            param2: 10.01
+            }
+        }
+    };
+console.log(struc.level1.level2.param1);
+console.log(struc.level1.level2.param2);
 
 /*
 Aufgabe 3 (3 Punkte)
-Wie nennt man die in Aufgabe 2 verwendete Struktur? Im Zusammenhang mit der
+a)Wie nennt man die in Aufgabe 2 verwendete Struktur? 
+b)Im Zusammenhang mit der
 Entwicklung von Webseiten mit HTML/JS ist Ihnen eine ähnliche Struktur bekannt -
-wie nennt man diese (Abkürzung) ? Wann wird diese Struktur vom Browser angelegt? 
+b1)wie nennt man diese (Abkürzung) ? b2) Wann wird diese Struktur vom Browser angelegt? 
 */
-
 /*
-Aufgabe 4 (16 Punkte)
-Legen Sie eine JS-Funktion rechner() an. Diese soll folgende Aufgaben erfüllen:
-Bei Aufruf sollen der Funktion zwei Zahlen übergeben werden. In Abhängigkeit eines dritten
-Parameters soll die Funktion entweder die Summe oder die Differenz der beiden Zahlen
-berechnen und danach das Ergebnis als String in der Form ‘Das Ergebnis lautet: XX’
-in die JS-Console ausgegeben. Verwenden Sie für die Funktionalität der Summen- bzw.
-Differenzbildung 2 Funktionen summe() und differenz()
-Geben Sie auch zwei Funktionscalls an! 
-*/
-
-/*
-
-*/
-
-/*
-
+Antwort zu 3a) Objekt
+Antwort zu 3b) 1. Document Object Modell oder DOM 2. beim parsen von HTML
 */
 
